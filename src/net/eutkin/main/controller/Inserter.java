2b450840eca0f5a2ec5b9ee90ba2cfa730ad6fc8
UtkinE <y_ne@bk.ru>
2015-04-05 13:11:40 +0700

    add comments in main class

--- src/net/eutkin/main/controller/Inserter.java
+++ src/net/eutkin/main/controller/Inserter.java
@@ -11,14 +11,23 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Pattern;
-
-
 import org.hibernate.JDBCException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * Responsibility:
+ * Insert new record in database
+ * from csv files
+ */
 public class Inserter{
-
-    private static Integer getNumberFromFileName(File currentFile, Boolean itIsTsNumber){
+    /**
+     * get id of traction substation
+     * or id meter
+     * @param   currentFile   current csv file
+     * @param   itIsTsNumber  flag, id TS or meter
+     * @return  id TS or meter
+     */
+    private static Integer getIdFromFileName(File currentFile, Boolean itIsTsNumber){
         String str = currentFile.getName();
         str = str.substring(6);
         Integer number = 0;
@@ -30,7 +39,12 @@ public class Inserter{
         }
         return number;
     }
-    private static Boolean correctNameFile(File file){
+    /**
+     * validate filename
+     * @param   file  current csv file
+     * @return  true, if filename is valid
+     */
+    private static Boolean validateFileName(File file){
         try {
             if(Pattern.matches(getPropFromProperties("PatternNameFile"),file.getName())) {
                 return true;
@@ -43,15 +57,20 @@ public class Inserter{
         }
         return false;
     }
-    private static Set<File> getSetFiles(String str) {
+    /**
+     * get list of csv file
+     * @param   path   path on csv file
+     * @return  set csv files
+     */
+    private static Set<File> getListFiles(String path) {
         LinkedHashSet<File> setFile = new LinkedHashSet<File>();
-        File oDirFile = new File(str);
+        File oDirFile = new File(path);
         try {
             for (File currentFile : oDirFile.listFiles()) {
                 if (currentFile.isFile()) {
                     setFile.add(currentFile);
                 } else if (currentFile.isDirectory()) {
-                    getSetFiles(currentFile.getAbsolutePath());
+                    getListFiles(currentFile.getAbsolutePath());
                 }
             }
         }
@@ -61,6 +80,12 @@ public class Inserter{
 
         return setFile;
     }
+    /**
+     * get property from file with properties
+     * @param   nameProperty
+     * @return  property value
+     * @throws  IOException
+     */
     private static String getPropFromProperties(String nameProperty) throws IOException{
         String prop = "";
         Properties property = new Properties();
@@ -74,8 +99,15 @@ public class Inserter{
         }
         return prop;
     }
+    /**
+     * get a row csv file, parse
+     * and fill fields entity (table)
+     * @param   list  row csv file
+     * @param   file  current csv file
+     * @return  entity
+     */
     private static AbstractDataTS fillFieldsDataMens(List<String> list,File file) {
-        EntityFactory entityFactory = new EntityFactory(getNumberFromFileName(file,true));
+        EntityFactory entityFactory = new EntityFactory(getIdFromFileName(file, true));
         AbstractDataTS dataTS1 = entityFactory.getEntity();
         if(list != null){
             String[] str = list.get(0).split(" ");
@@ -117,20 +149,26 @@ public class Inserter{
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            dataTS1.setMeter_id(getNumberFromFileName(file, false));
+            dataTS1.setMeter_id(getIdFromFileName(file, false));
         } else
             return null;
         return dataTS1;
     }
+    /**
+     * get config Spring Framework,
+     * call method for getting list file,
+     *
+     * @param args
+     */
     public static void main (String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
         IDataServiceTest<AbstractDataTS> dataServiceTest = (IDataServiceTest) ctx.getBean("entityService");
         try {
-            LinkedHashSet<File> setFile = new LinkedHashSet<File>(getSetFiles(getPropFromProperties("Path")));
+            LinkedHashSet<File> setFile = new LinkedHashSet<File>(getListFiles(getPropFromProperties("Path")));
             Iterator<File> iterator = setFile.iterator();
             while (iterator.hasNext()) {
                 File currentFile = iterator.next();
-                if (!correctNameFile(currentFile)){System.out.println("ERROR: Incorrect file name");continue;}
+                if (!validateFileName(currentFile)){System.out.println("ERROR: Incorrect file name");continue;}
                 Csv.Reader oReader = new Csv.Reader(new FileReader(currentFile)).delimiter(';').ignoreComments(true);
                 while(true) try {
                     List<String> lineCSV = oReader.readLine();
@@ -158,81 +196,3 @@ public class Inserter{
 
 
 }
-
-/*
-@Controller
-public class Inserter {
-
-    //@Autowired
-    //private IDataMensService dataMensService;
-
-    public void parseLine(List<String> line) throws Exception {
-        IDataMensService dataMensService = new DataMensServiceImpl();
-        List<String> lineCSV = new LinkedList<String>(line);
-        ts1 dataMens = new ts1();
-        dataMens.setTs_id(1);
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss");
-            Date oDate = sdf.parse(lineCSV.get(0));
-            dataMens.setTimeMensuration(oDate);
-        } catch (Exception e){
-            e.printStackTrace();
-        } try {
-            Double oDouble = Double.parseDouble(lineCSV.get(1));
-            dataMens.setVoltage(oDouble);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } try {
-            Double oDouble = Double.parseDouble(lineCSV.get(2));
-            dataMens.setThe_current(oDouble);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } try {
-            Double oDouble = Double.parseDouble(lineCSV.get(3));
-            dataMens.setPower(oDouble);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } try {
-            Double oDouble = Double.parseDouble(lineCSV.get(4));
-            dataMens.setGiven_energy(oDouble);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } try {
-            Double oDouble = Double.parseDouble(lineCSV.get(5));
-            dataMens.setAccepted_energy(oDouble);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } try {
-            dataMensService.insertNewLine(dataMens);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public static void main(String args[]) throws Exception {
-        String path = "E:\\1.csv";
-        Inserter inserter = new Inserter();
-        File oFile = new File(path);
-        try {
-            Csv.Reader oReader = new Csv.Reader(new FileReader(oFile))
-                    .delimiter(';').ignoreComments(true);
-
-            //while (true) {
-            List<String> oListLine = oReader.readLine();
-            if (oListLine != null) {
-                System.out.println(oListLine);
-                inserter.parseLine(oListLine);
-            }
-            else {
-                //break;
-            }
-            //}
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-}
-*/
