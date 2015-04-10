1b352ea373e9785cf988042029bb69d68b282c67
UtkinE <y_ne@bk.ru>
2015-04-10 17:45:49 +0700

    before multithreads

--- src/net/eutkin/main/factory/EntityFactory.java
+++ src/net/eutkin/main/factory/EntityFactory.java
@@ -1,9 +1,7 @@
 package net.eutkin.main.factory;
 
 
-import net.eutkin.main.entity.AbstractDataTS;
-import net.eutkin.main.entity.DataTS1;
-import net.eutkin.main.entity.DataTS2;
+import net.eutkin.main.entity.*;
 
 public class EntityFactory {
     private int numTS;
@@ -14,6 +12,10 @@ public class EntityFactory {
         switch (this.numTS){
             case 1: return new DataTS1();
             case 2: return new DataTS2();
+            case 3: return new DataTS3();
+            case 4: return new DataTS4();
+            case 5: return new DataTS5();
+            case 6: return new DataTS6();
         }
         return new DataTS1();
     }
